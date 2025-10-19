9502b18f0bc091723302be2f9259625f7a4a0125
yannamsiva <46184940+yannamsiva@users.noreply.github.com>
2025-10-19 16:28:06 +0530

    Update FixedColorfulCalculator.java

--- src/main/java/com/shiva/FixedColorfulCalculator.java
+++ src/main/java/com/shiva/FixedColorfulCalculator.java
@@ -2,7 +2,7 @@ package com.shiva;
 
 import java.util.Scanner;
 
-public class ConsoleCalculator {
+public class FixedColorfulCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
